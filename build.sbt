f16149d93bd5e37ebf7aec2aba866539c8455611
Edwin Gigi <P2508298@my365.dmu.ac.uk>
2022-02-09 09:52:08 +0000

    Initial commit and first 'Hello World' Scala Program

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala-FP CTEC3904"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
---
+++ src/main/scala/Topic_1_Getting_Started/Hello.scala
@@ -0,0 +1,6 @@
+package Topic_1_Getting_Started
+
+//My First Scala Program
+object Hello extends App {
+  println("Hello, World!")
+}
